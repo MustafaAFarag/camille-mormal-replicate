aa9ab715642cd50baf26f3bb542536cf8796ce69
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-10-11 10:31:48 +0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import CrossCursor from "./components/CrossCursor";
 import ImageTrack from "./components/ImageTrack";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <main>
+      <Navbar />
       <CrossCursor />
       <ImageTrack></ImageTrack>
     </main>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import "../styles/navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="nav">
+      <ul className="list">
+        <li className="list-item">Work</li>
+        <li className="list-item">About</li>
+      </ul>
+    </div>
+  );
+}
